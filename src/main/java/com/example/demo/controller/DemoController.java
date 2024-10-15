3c8a5afe872887ccdcc6979249b0cc610a89ef57
amatna <hmin220@naver.com>
2024-10-15 17:15:28 +0900

    4 week

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,61 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.model.domain.TestDB;
+import com.example.demo.model.service.TestService;
+
+@Controller // 컨트롤러 어노테이션 명시
+public class DemoController
+{
+    @Autowired
+    TestService testService;
+    
+    @GetMapping("/hello") // 전송 방식 GET
+    public String hello(Model model) {
+        model.addAttribute("data", "방갑습니다."); // model 설정
+        return "hello"; // hello.html 연결  // 이 문자열을 기반으로 viewResolver에 넘겨서 hello.html가 렌더링된걸 view에 출력
+    }
+
+    @GetMapping("/hello2")
+    public String hello2(Model model) {
+        model.addAttribute("data2", "홍길동님.");
+        model.addAttribute("data2", "방갑습니다.");
+        model.addAttribute("data3", "오늘.");
+        model.addAttribute("data4", "날씨는.");
+        model.addAttribute("data5", "매우 좋습니다.");
+        return "hello2";
+    }
+
+    @GetMapping("/about_detailed")
+    public String about() {
+        return "about_detailed";
+    }
+
+    @GetMapping("/thymeleaf_test1")
+        public String thymeleaf_test1(Model model) {
+        model.addAttribute("data1", "<h2> 방갑습니다 </h2>");
+        model.addAttribute("data2", "태그의 속성 값");
+        model.addAttribute("link", 01);
+        model.addAttribute("name", "홍길동");
+        model.addAttribute("para1", "001");
+        model.addAttribute("para2", 002);
+        return "thymeleaf_test1";
+    }
+
+    @GetMapping("/testdb")
+    public String getAllTestDBs(Model model) {
+        TestDB test = testService.findByName("홍길동");
+        model.addAttribute("data4", test);
+        System.out.println("데이터 출력 디버그 : " + test);
+        return "testdb";
+    }
+
+    // @GetMapping("/article_list")
+    // public String article_list() {
+    //     return "article_list";
+    // }
+}
